2efa6a14812f85331f6bdec965835536139f7cd8
Devansh <dkansagra04@gmail.com>
2023-08-13 21:53:07 +0530

    Added React Router to the website

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import About from './components/About';
+import Contact from './components/Contact';
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
@@ -16,13 +19,19 @@ function App() {
     setTimeout(() => {
       setalert(null);
     }, 1500);
-  } 
+  }
   return (
     <>
-      <Navbar title="Text-Utils" showAlert={showAlert}/>
-      <Alert alert={alert}/>
+      <Navbar title="Text-Utils" showAlert={showAlert} />
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze" showAlert={showAlert}/>
+          <Routes>
+            <Route path="/" element={<TextForm heading="Enter the text to analyze" showAlert={showAlert} />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        {/* <TextForm heading="Enter the text to analyze" showAlert={showAlert}/>
+        <About /> */}
       </div>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 export default function Navbar(props) {
 
@@ -19,23 +20,29 @@ export default function Navbar(props) {
     return (
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary" id="nav">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="/">{props.title}</a>
+                <div className="container d-flex justify-content-between">
+                    <div className="navbar-brand">{props.title}</div>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/about">About</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/contact">Contact Us</Link>
                             </li>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <div className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Change Theme
-                                </a>
+                                </div>
                                 <ul className="dropdown-menu">
-                                    <li><button className="dropdown-item" href="/" onClick={chToLight}>Light</button></li>
-                                    <li><button className="dropdown-item" href="/" onClick={chToDark}>Dark</button></li>
+                                    <li><button className="dropdown-item" onClick={chToLight}>Light</button></li>
+                                    <li><button className="dropdown-item" onClick={chToDark}>Dark</button></li>
                                 </ul>
                             </li>
                         </ul>
